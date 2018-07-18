ea55ac011a71dcee71e991fa7319510c7d57f94d
y-aakarsh <aakarsh@xelpmoc.in>
2018-07-18 18:02:50 +0530

    Adding git ignore

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+import * as React from "react"
+export interface HelloProps {compiler:string,framework:string;}
+export const Hello = (props:HelloProps) => <h1>Hellow  from {props.compiler} and {props.framework}</h1>
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import {Hello} from "./App"
+ReactDOM.render(<Hello compiler="Aakarsh" framework="yadav" />,document.getElementById("example"))
